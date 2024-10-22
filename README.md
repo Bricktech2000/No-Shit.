4cd0f7c4b0835d0da1301d423bec0c30dd9d89cd
Emilien Breton <bricktech2000@gmail.com>
2024-10-21 20:52:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# No Shit.
+
+_An opinionated subset of `-Wall -Wextra`_
+
+> warning: adding 'int' to a string does not append to the string
+
+No shit.
+
+This set of flags is designed around compiling C under GCC and Clang. Our guiding principle is to silence any warning aimed at flagging code that possibly contains a programmer error. Note that some warnings, such as `-Wunused` and `-Wswitch`, fit this description but are currently not silenced.
---
+++ no-shit.sh
@@ -0,0 +1,36 @@
+# when adding flags make sure to `vipk<cmd>sort<cr>`
+export CFLAGS="
+-Wall
+-Wextra
+-Wno-unknown-warning-option
+
+-Wdangling-pointer=1
+-Wno-bool-operation
+-Wno-char-subscripts
+-Wno-comment
+-Wno-for-loop-analysis
+-Wno-format-contains-nul
+-Wno-format-extra-args
+-Wno-format-security
+-Wno-format-zero-length
+-Wno-implicit-fallthrough
+-Wno-int-in-bool-context
+-Wno-logical-not-parentheses
+-Wno-maybe-uninitialized
+-Wno-misleading-indentation
+-Wno-missing-field-initializers
+-Wno-multichar
+-Wno-multistatement-macros
+-Wno-parentheses
+-Wno-self-assign
+-Wno-sign-compare
+-Wno-sizeof-array-argument
+-Wno-sizeof-array-decay
+-Wno-sizeof-array-div
+-Wno-sizeof-pointer-div
+-Wno-sizeof-pointer-memaccess
+-Wno-string-plus-int
+-Wno-trigraphs
+-Wno-unused-parameter
+-Wuse-after-free=1
+"
